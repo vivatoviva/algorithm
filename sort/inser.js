82044a243a196178df650cac99e5df4cd18a4e1c
vivatoviva <17051298278@163.com>
2018-12-04 22:06:03 +0800

    insert

--- sort/inser.js
+++ sort/inser.js
@@ -1,4 +1,5 @@
 
+// 实现方式 1
 function each(arr, i, j) {
   let mid = arr[i];
   arr[i] = arr[j];
@@ -15,3 +16,18 @@ function sort(arr) {
 const arr = [1,2,4,5,3,2,6,3,1];
 sort(arr);
 console.log(arr);
+
+// 实现方式二
+function sort2 (arr) {
+  for (let i = 1, len = arr.length; i < len; i++) {
+    const x = arr[i];
+    let j = i - 1;
+    while(j >= 0 && x < arr[j]) {
+      arr[j+1] = arr[j];
+      j--;
+    }
+    arr[j+1] = x;
+  }
+  return arr;
+}
+console.log(sort2([2,1,3,4,2,6,7,3]))
