ade4f7c8218cb622bd1cfbcec073ac7b6e0914fc
vivatoviva <17051298278@163.com>
2018-12-06 23:48:38 +0800

    offer

--- offer/斐波那契数列.js
+++ offer/斐波那契数列.js
@@ -1,21 +1,22 @@
-// 缓存处理相关计算结果
-// function Fibonacci(index)
-// {
-//     // write code here
-//     const cache = {};
-//     function invoke(n) {
-//         if( n=== 0) return 0;
-//         if (n === 1 || n === 2) return 1;
-//         if(cache[n]) return cache[n];
-//         cache[n - 1] = invoke(n - 1);
-//         cache[n - 2] = invoke(n - 2);
-//         return cache[n - 1] + cache[n - 2];
-//     }
-//     return invoke(index);
-// }
-// console.log(Fibonacci(100));
+// 缓存处理相关计算结果 递归加循环
+function Fibonacci(index)
+{
+    // write code here
+    const cache = {};
+    function invoke(n) {
+        if( n=== 0) return 0;
+        if (n === 1 || n === 2) return 1;
+        if(cache[n]) return cache[n];
+        cache[n - 1] = invoke(n - 1);
+        cache[n - 2] = invoke(n - 2);
+        return cache[n - 1] + cache[n - 2];
+    }
+    return invoke(index);
+}
 
-// 循环版本
+console.log(Fibonacci(100));
+
+// 迭代循环版本
 function Fibonacci(n) {
   let i = 0;
   let j = 1;
@@ -25,5 +26,4 @@ function Fibonacci(n) {
   }
   return i;
 }
-
-console.log(Fibonacci(3))
+console.log(Fibonacci(100))
